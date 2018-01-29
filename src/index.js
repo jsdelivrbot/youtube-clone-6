2e073d950057a48acd182af3a30157765b23c49a
tylerwarren <tylerleewarren@gmail.com>
2018-01-29 15:51:43 -0500

    Render VideoDetail component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
@@ -22,6 +23,7 @@ class App extends Component {
     return ( 
       <div>
         <SearchBar />
+        <VideoDetail video={ this.state.videos[0] }/>
         <VideoList videos={ this.state.videos } />
       </div>
     );  

bbaa88c112e5b2730309c4c5599b07aa8b1794b8
tylerwarren <tylerleewarren@gmail.com>
2018-01-29 16:28:28 -0500

    Hook up SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
@@ -17,7 +18,11 @@ class App extends Component {
       selectedVideo: null 
     };
 
-    YTSearch({ key: YOUTUBE_API_KEY, term: 'surboards' }, (videos) => {
+    this.videoSearch('skateboarding');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: YOUTUBE_API_KEY, term: term }, videos => {
       this.setState({ 
         videos: videos,
         selectedVideo: videos[0]
@@ -26,12 +31,14 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
+
     return ( 
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={ videoSearch }/>
         <VideoDetail video={ this.state.selectedVideo }/>
         <VideoList
-          onVideoSelect={ (selectedVideo) => this.setState({ selectedVideo }) }
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
           videos={ this.state.videos } 
         />
       </div>
